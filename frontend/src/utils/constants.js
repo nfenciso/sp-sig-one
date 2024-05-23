249d892fbb585e844b26c4ed234eedc9643383bc
nfenciso <nfenciso@up.edu.ph>
2024-05-24 00:16:32 +0800

    fix2: attempt 2

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
 import { get } from "lodash";
 
-const baseURL = get(process.env, "REACT_APP_BACKEND_URL", "http://localhost:5000");
-const selfURL = get(process.env, "REACT_APP_FRONTEND_URL", "http://localhost:3000");
+var baseURL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";// get(process.env, "REACT_APP_BACKEND_URL", "http://localhost:5000");
+var selfURL = process.env.REACT_APP_FRONTEND_URL || "http://localhost:3000";
 
 export { baseURL, selfURL };
