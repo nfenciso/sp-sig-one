b6cdc1c87a6347974c15186da7bd77e279af762d
nfenciso <nfenciso@up.edu.ph>
2024-05-24 00:00:19 +0800

    build: set to allow for both local and deployed run

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
 import { get } from "lodash";
 
-const baseURL = "https://sp-sig-one-backend.vercel.app";//get(process.env, "REACT_BACKEND_URL", "http://localhost:5000");
-const selfURL = "https://sp-sig-one.vercel.app";
+const baseURL = get(process.env, "REACT_BACKEND_URL", "http://localhost:5000");
+const selfURL = get(process.env, "REACT_FRONTEND_URL", "http://localhost:3000");
 
 export { baseURL, selfURL };
