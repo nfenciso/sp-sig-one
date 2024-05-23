38d0efdf8173cd55278a8f6f88d28ea34d7174fa
nfenciso <nfenciso@up.edu.ph>
2024-05-24 00:10:29 +0800

    fix: add _APP to react env names

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
 import { get } from "lodash";
 
-const baseURL = get(process.env, "REACT_BACKEND_URL", "http://localhost:5000");
-const selfURL = get(process.env, "REACT_FRONTEND_URL", "http://localhost:3000");
+const baseURL = get(process.env, "REACT_APP_BACKEND_URL", "http://localhost:5000");
+const selfURL = get(process.env, "REACT_APP_FRONTEND_URL", "http://localhost:3000");
 
 export { baseURL, selfURL };
